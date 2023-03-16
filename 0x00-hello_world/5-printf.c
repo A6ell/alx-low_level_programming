7a7603ec08a4f7c041b2b3e41c228cc55063540b
Ab-Ruh <abell.demere@gmail.com>
2023-03-16 19:29:43 +0300

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * Description:'using printf'
+ * Parameters:no input returns an int
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	printf("%s", "Roses are red,\n");
+
+	printf("%s", "Violets are blue,\n");
+
+	printf("%s", "I'm terrible at poetry,\n");
+
+	printf("%s", "But my grammar's on point too.\n");
+
+	return (0);
+}
