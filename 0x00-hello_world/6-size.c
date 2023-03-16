9c442b8603be7eb1855f7b645b1b265e23621733
Ab-Ruh <abell.demere@gmail.com>
2023-03-16 20:29:26 +0300

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	printf("size of an int: %u byte(s)\n", sizeof(int));
 	printf("size of a long int: %u bytes(s)\n", sizeof(long int));
 	printf("size of a long long int: %u bytes(s)\n", sizeof(long long int));
-	printf("size of float: %u byte(s)\n", sizeof(float));
+	printf("size of a float: %u byte(s)\n", sizeof(float));
 
 	return (0);
 }
