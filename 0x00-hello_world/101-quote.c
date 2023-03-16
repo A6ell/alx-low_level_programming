bd5ee64f8556a5a794db68d898430cea0198e28c
Ab-Ruh <abell.demere@gmail.com>
2023-03-16 22:10:46 +0300

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,12 @@
  * Parameters:no input returns an int
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        write(2, msg, sizeof(msg) - 1);
-        return (1);
+
+	write(2, msg, sizeof(msg) - 1);
+
+	return (1);
 }
