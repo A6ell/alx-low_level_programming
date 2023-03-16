b3e1fd694a7b35b6bb058465f6111e1212f6fb12
Ab-Ruh <abell.demere@gmail.com>
2023-03-16 19:13:09 +0300

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
- *
+ * Description:'printing using puts'
+ * Parameters:no input returns an int
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
-    char*s=\"Programming is like building a multilingual puzzle";
-    puts(s);
-    return (0);
+	char *s = "\"Programming is like building a multilingual puzzle";
+
+	puts(s);
+
+	return (0);
 }
