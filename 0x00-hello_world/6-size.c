fb49835fbaa213e87057130c45f9059e7c86964a
Ab-Ruh <abell.demere@gmail.com>
2023-03-16 20:38:35 +0300

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- * Description:'using sizeof various variable types'
- * Parameters:no input returns an int
- * Return: Always 0 (success)
- */
-
+* main - Entry point
+* Description: 'printing sizes of various types'
+* Parameters: no input returns an int
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("size of a char: %u byte(s)\n", sizeof(char));
-	printf("size of an int: %u byte(s)\n", sizeof(int));
-	printf("size of a long int: %u bytes(s)\n", sizeof(long int));
-	printf("size of a long long int: %u bytes(s)\n", sizeof(long long int));
-	printf("size of a float: %u byte(s)\n", sizeof(float));
-
-	return (0);
+        printf("Size of a char: %u byte(s)\n", sizeof(char));
+        printf("Size of an int: %u byte(s)\n", sizeof(int));
+        printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+        printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+        printf("Size of a float: %u byte(s)\n", sizeof(float));
+        return (0);
 }
