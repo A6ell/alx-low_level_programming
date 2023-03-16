3319ad5e836548deeb07b25ee593bff69d5c777e
Ab-Ruh <abell.demere@gmail.com>
2023-03-16 21:38:13 +0300

    8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,18 @@
 #include <unistd.h>
 /**
  * main - Entry point of the program
- * Description:'using printf'
+ * Description:'using const char'
  * Parameters:no input returns an int
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, msg, sizeof(msg) - 1);
+	const char msg[] = "art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	ssize_t len = sizeof(msg);
+
+	write(STDERR_FILENO, msg, len);
 
 	return (1);
 }
