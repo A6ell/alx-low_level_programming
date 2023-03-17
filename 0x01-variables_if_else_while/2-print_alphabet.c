40501f03784e5282123170bddbd2b75ea6fff936
Ab-Ruh <abell.demere@gmail.com>
2023-03-17 19:26:01 +0300

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ * Description:'using printf'
+ * Parameters:no input returns an int
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	do
+
+	{
+		putchar(ch);
+		ch++;
+	} while (ch <= 'z');
+	putchar('\n');
+	return (0);
+}
