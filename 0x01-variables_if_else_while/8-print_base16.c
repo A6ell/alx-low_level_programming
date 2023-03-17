d9af6fa77ee7da256f79d158dee315208c2a2e5b
Ab-Ruh <abell.demere@gmail.com>
2023-03-17 20:14:05 +0300

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ * Description:'using printf'
+ * Parameters:no input returns an int
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + 48);
+		}
+		else
+		{
+			putchar(i + 87);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
