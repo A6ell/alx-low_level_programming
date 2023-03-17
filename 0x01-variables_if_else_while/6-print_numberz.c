18c697b8aa6948f8f808b4b17a904e8354f381cb
Ab-Ruh <abell.demere@gmail.com>
2023-03-17 20:01:00 +0300

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ * Description:'using printf'
+ * Parameters:no input returns an int
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
+	putchar('\n');
+	return (0);
+}
