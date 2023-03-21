444fb27d8b5dd7b8653bf2bd73ad6e4f065f84a1
Ab-Ruh <abell.demere@gmail.com>
2023-03-21 22:39:30 +0300

    8

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ * */
+
+int add(int, int)
+
+{
+    int a = 2;
+    int b = 3;
+    int result = add(a, b);
+    return (result);
+}
