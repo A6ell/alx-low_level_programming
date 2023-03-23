e2098dbfc54e1bc2162b07f2d8e7d1fd51051751
Ab-Ruh <abell.demere@gmail.com>
2023-03-23 17:38:38 +0300

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_diagonal - is a function that prints numbers
+ *@n: number of lines
+ *Return: the list of numbers in new line
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < n; i++)
+	{
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
