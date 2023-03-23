e153055034c3e1c132b87c17157ab7f190e076c9
Ab-Ruh <abell.demere@gmail.com>
2023-03-23 13:38:23 +0300

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - returns 1 if the character is upercase
- * @c: third integer
- * Return: 1
+ * @c: the character to check
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
 {
-	if (_isupper(c))
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
