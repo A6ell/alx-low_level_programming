d3e31518588c1a8a35a367c6f75c47e8611d828e
Ab-Ruh <abell.demere@gmail.com>
2023-03-23 15:15:01 +0300

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - is a function that prints numbers from 0 to 9
+ *
+ *Return: the list of numbers in new line
+ */
+
+int print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	return (i);
