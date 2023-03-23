f2737f6031c8cc8ed89459ea445c77dad88cfa86
Ab-Ruh <abell.demere@gmail.com>
2023-03-23 19:28:34 +0300

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i < 15; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			putchar(j + '0');
-		}
-		putchar('\n');
-	}
+			}
+			putchar('\n');
+			}
 }
