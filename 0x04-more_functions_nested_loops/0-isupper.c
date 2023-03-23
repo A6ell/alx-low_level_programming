1bc6d9716995f282411bf8273577725e71e07b50
Ab-Ruh <abell.demere@gmail.com>
2023-03-23 13:23:09 +0300

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - returns 1 if the character is upercase
+ * @c: third integer
+ * Return: 1
+ */
+
+int _isupper(int c)
+{
+	if (_isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
