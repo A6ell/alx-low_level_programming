b81520b165d617d5b87cd47753795808f510a6c5
Ab-Ruh <abell.demere@gmail.com>
2023-03-27 16:58:05 +0300

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,15 @@
+/**
+ * reset_to_98 - function
+ * Description: change value using pointer
+ * Parameters: int size input no return
+ * @n: integer
+ * Return: n
+ */
+
 #include "main.h"
 void reset_to_98(int *n)
 {
-    int *p = &n;
-    
-    *p = 98;
+	int *p = &n;
+
+	*p = 98;
 }
