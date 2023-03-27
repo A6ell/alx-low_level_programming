0392620e56d5c5b203f700e5cbc1ca9cd2fb8a1b
Ab-Ruh <abell.demere@gmail.com>
2023-03-27 21:27:42 +0300

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,9 +9,13 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; *str != 0; i++)
 	{
-		putchar(str[i]);
+	if (i % 2 == 0)
+	{
+		putchar(*str);
+	}
+	str++;
 	}
 	putchar('\n');
 }
