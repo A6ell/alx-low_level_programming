bc6ad147867bf95fb42c3bd17952586f54752ef0
Ab-Ruh <abell.demere@gmail.com>
2023-03-27 20:21:19 +0300

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,13 +4,14 @@
  * puts2 - prints every other character of a string
  * @str: the string to print
  */
+
 void puts2(char *str)
 {
-    int i;
+	int i;
 
-    for (i = 0; str[i] != '\0'; i += 2)
-    {
-        putchar(str[i]);
-    }
-    putchar('\n');
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
 }
