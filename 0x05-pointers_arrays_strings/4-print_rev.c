4555bdfc9ddd7a8f832d731b8a0ee3612ee16cea
Ab-Ruh <abell.demere@gmail.com>
2023-03-27 20:03:56 +0300

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,16 +6,11 @@
  */
 void print_rev(char *s)
 {
-    int len = 0, i;
+	int len = 0, i;
 
-    /* calculate the length of the string */
-    while (s[len] != '\0')
-        len++;
-
-    /* print the string in reverse order */
-    for (i = len - 1; i >= 0; i--)
-        _putchar(s[i]);
-
-    /* print a new line */
-    _putchar('\n');
+	while (s[len] != '\0')
+		len++;
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
