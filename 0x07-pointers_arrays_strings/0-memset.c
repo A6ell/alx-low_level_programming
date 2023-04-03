95edeca70862bea96532c15340df47b0e8b095fe
Ab-Ruh <abell.demere@gmail.com>
2023-04-03 23:12:00 +0300

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * _memset - Entry
- * Description: 'replacing memories with smt'
- * Parameters: returns a char
+ * _memset - function
  * @s: char 1
  * @b: char 2
  * @n: input
@@ -12,6 +10,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	char *result;
 
 	for (i = 0; i < n; i++)
