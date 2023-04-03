05dd59ded04e8a1fd5a8ac0e41d6e57d932fe2d4
Ab-Ruh <abell.demere@gmail.com>
2023-04-03 19:28:48 +0300

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,11 @@
 /**
- * _memset - Set momery
- * @s: pionter
- * @b: char
- * @n: int
- * Return: s
+ * _memset - Set memory
+ * Description: 'replace with smt'
+ * Parameters: two char then an input to return char
+ * @s: char 1
+ * @b: char 2
+ * @n: int in
+ * Return: modified Char
 */
 
 char *_memset(char *s, char b, unsigned int n)
