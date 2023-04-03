2dfd0a64453907b032d0333f2a68832f6d0e9401
Ab-Ruh <abell.demere@gmail.com>
2023-04-03 14:11:18 +0300

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+/**
+ * _strspn - Function
+ * @s: Char
+ * @accept: Char
+ * Return: i
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
