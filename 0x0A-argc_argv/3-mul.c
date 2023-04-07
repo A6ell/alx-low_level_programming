fae9f50823b7b9114ce93656229dc389000e8718
Ab-Ruh <abell.demere@gmail.com>
2023-04-07 18:50:50 +0300

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,12 +1,19 @@
+/**
+ * main - prints out multiple of 2 argument user inputs
+ * @argv: prints out strings of input
+ * @argc: counts the number of input
+ * Return: 0
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-    printf("%s\n","Error");
-    return (1);
-    }
-    printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-    return (0);
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
