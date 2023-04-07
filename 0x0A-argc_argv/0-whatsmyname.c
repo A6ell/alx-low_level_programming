956df3ed64ac177d5394b68dd592623558b78220
Ab-Ruh <abell.demere@gmail.com>
2023-04-07 15:56:59 +0300

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+/**
+ * main - prints out name of file and its path
+ * @argc: counts the number of input
+ * @argv: array the first being the programs name and file path
+ * Return: 0
+ */
+
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
