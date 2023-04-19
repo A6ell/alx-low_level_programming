5bf3162dba47ef4055d1e99be0b91fe25ba24fef
Ab-Ruh <abell.demere@gmail.com>
2023-04-19 18:35:00 +0300

    0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,13 @@
 #include <stddef.h>
 /**
-* print_name - prints a name in a specific format.
-* @name: name of the person
-* @f: pointer to a function that takes a char pointer and returns void
-*
-* Return: Nothing.
-*/
+ * print_name - prints a name in a specific format.
+ * @name: name of the person
+ * @f: pointer to a function that takes a char pointer and returns void
+ *
+ * Return: Nothing.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && f != NULL)
-f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
