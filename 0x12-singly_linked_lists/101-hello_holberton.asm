d6e6ca174e2c4cba8979eee6480b28abac11f608
Ab-Ruh <abell.demere@gmail.com>
2023-04-27 18:40:26 +0300

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,15 @@
 section .data
-    message db "Hello, Holberton", 10 ; the message to print
+message db "Hello, Holberton", 10 ;  the message to print
 
 section .text
-    global _start
+global _start
 _start:
-    ; call the printf function to print the message
-    push message
-    call printf
-    add rsp, 8
+;  call the printf function to print the message
+push message
+call printf
+add rsp, 8
 
-    ; call the exit function to terminate the program
-    xor edi, edi ; return code 0
-    mov eax, 60 ; exit syscall number
-    syscall
+;  call the exit function to terminate the program
+xor edi, edi ;  return code 0
+mov eax, 60 ;  exit syscall number
+syscall
