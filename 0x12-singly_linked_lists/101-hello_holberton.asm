54ac16cc02f8f5e24dbaff5921874cafe99f94a3
Ab-Ruh <abell.demere@gmail.com>
2023-04-27 18:37:48 +0300

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,15 @@
 section .data
-    hello db 'Hello, Holberton', 10  ; 10 is ASCII code for newline character
-    hello_len equ $ - hello         ; length of hello string
+    message db "Hello, Holberton", 10 ; the message to print
 
 section .text
     global _start
-
 _start:
-    ; write hello string to stdout
-    mov eax, 4          ; system call for write
-    mov ebx, 1          ; file descriptor for stdout
-    mov ecx, hello      ; address of hello string
-    mov edx, hello_len  ; length of hello string
-    int 0x80            ; call kernel to perform system call
+    ; call the printf function to print the message
+    push message
+    call printf
+    add rsp, 8
 
-    ; exit program
-    mov eax, 1          ; system call for exit
-    xor ebx, ebx        ; exit status code
-    int 0x80            ; call kernel to perform system call
+    ; call the exit function to terminate the program
+    xor edi, edi ; return code 0
+    mov eax, 60 ; exit syscall number
+    syscall
