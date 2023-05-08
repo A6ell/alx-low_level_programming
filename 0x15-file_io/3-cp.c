6cce10d4742818b5485ced41bbdc5b75dbcb908c
Ab-Ruh <abell.demere@gmail.com>
2023-05-08 22:14:12 +0300

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,7 +1,6 @@
 /**
 * main - Entry point. Copies the content of a file to another file
-* @ac: Argument count
-* @av: Array of arguments
+*
 * Return: 0 on success, otherwise 97, 98, 99, or 100 on error
 */
 
@@ -12,85 +11,93 @@
 #include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
-/**
-* copy_file - Copies the content of a file to another file
-* @file_from: Source file
-* @file_to: Destination file
-* Return: 0 on success, otherwise 97, 98, 99, or 100 on error
-*/
-int copy_file(const char *file_from, const char *file_to)
-{
-int fd_from, fd_to, read_bytes, write_bytes; 
-char buffer[BUFFER_SIZE]; 
-mode_t mode; 
 
-mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH; 
-
-fd_from = open(file_from, O_RDONLY); 
-if (fd_from == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from); 
-return (98); 
-}
-
-fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, mode); 
-if (fd_to == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to); 
-close(fd_from); 
-return (99); 
-}
-
-while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-{
-write_bytes = write(fd_to, buffer, read_bytes); 
-if (write_bytes != read_bytes)
-{
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to); 
-close(fd_from); 
-close(fd_to); 
-return (99); 
-}
-}
-
-if (read_bytes == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from); 
-close(fd_from); 
-close(fd_to); 
-return (98); 
-}
-
-if (close(fd_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from); 
-close(fd_to); 
-return (100); 
-}
-
-if (close(fd_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to); 
-return (100); 
-}
-
-return (0); 
-}
+int open_file(const char *filename, int flags, mode_t mode);
+void close_file(int fd);
+void copy_file(const char *from, const char *to);
 
 /**
-* main - Entry point.
+* main - Entry point. Copies the content of a file to another file
 * @ac: Argument count
 * @av: Array of arguments
+*
 * Return: 0 on success, otherwise 97, 98, 99, or 100 on error
 */
 int main(int ac, char **av)
 {
 if (ac != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"); 
-return (97); 
+fprintf(stderr, "Usage: cp file_from file_to\n");
+exit(EXIT_FAILURE);
 }
 
-return (copy_file(av[1], av[2])); 
+copy_file(av[1], av[2]);
+
+return (0);
 }
 
+/**
+* open_file - Opens a file with the given filename, flags, and mode.
+* @filename: The name of the file to open.
+* @flags: The flags to use when opening the file.
+* @mode: The mode to use when creating the file.
+*
+* Return: The file descriptor of the opened file.
+*/
+int open_file(const char *filename, int flags, mode_t mode)
+{
+int fd = open(filename, flags, mode);
+if (fd == -1)
+{
+fprintf(stderr, "Error: Can't open file %s\n", filename);
+exit(EXIT_FAILURE);
+}
+return (fd);
+}
+
+/**
+* close_file - Closes the file with the given file descriptor.
+* @fd: The file descriptor of the file to close.
+*/
+void close_file(int fd)
+{
+if (close(fd) == -1)
+{
+fprintf(stderr, "Error: Can't close fd %d\n", fd);
+exit(EXIT_FAILURE);
+}
+}
+
+/**
+* copy_file - Copies the contents of one file to another.
+* @from: The name of the file to copy from.
+* @to: The name of the file to copy to.
+*/
+void copy_file(const char *from, const char *to)
+{
+int fd_from = open_file(from, O_RDONLY, 0);
+int fd_to = open_file(to, O_WRONLY | O_CREAT | O_TRUNC,
+S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+
+char buffer[BUFFER_SIZE];
+ssize_t nread;
+
+while ((nread = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+{
+ssize_t nwritten = write(fd_to, buffer, nread);
+if (nwritten == -1 || nwritten != nread)
+{
+fprintf(stderr, "Error: Can't write to file %s\n", to);
+exit(EXIT_FAILURE);
+}
+}
+
+if (nread == -1)
+{
+fprintf(stderr, "Error: Can't read from file %s\n", from);
+exit(EXIT_FAILURE);
+}
+
+close_file(fd_from);
+close_file(fd_to);
+}
