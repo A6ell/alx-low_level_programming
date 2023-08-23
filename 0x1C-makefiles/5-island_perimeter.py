7deeee5e54482a0334636058633190ff9cba73fd
Ab-Ruh <abell.demere@gmail.com>
2023-08-23 18:56:58 +0300

    makefile

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Function that returns the perimeter of an island.
+"""
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid.
+    Args:
+        grid (list): Nested list of integers with 0s (water) and 1s (land).
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+
+            if grid[i][j] == 1:
+
+                # Check top edge
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+
+                # Check bottom edge
+                if i == len(grid)-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+
+                # Check left edge
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+
+                # Check right edge
+                if j == len(grid[i])-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+
+    return perimeter
